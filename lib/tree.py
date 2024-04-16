856aba993bd88b10d33c2bef1e44ba5b3da262b6
Timothy Ryon <tim@timryon.com>
2024-04-15 22:21:47 -0400

    finish traverse breadth and pass test

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,25 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    result = list()
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop(0)
+      if node["id"] == id:
+        return node
+      else:
+        result.append(node["id"])
+      nodes_to_visit = nodes_to_visit + node["children"]
+
+  # def get_element_by_id(self, id, node):
+  #   result = list()
+  #   nodes_to_visit = [node]
+
+  #   while len(nodes_to_visit) > 0:
+  #     node = nodes_to_visit.pop[0]
+  #     if node["id"] == id:
+  #       return node
+  #     else:
+  #       result.append(node["text_content"])
+  #     nodes_to_visit = nodes_to_visit + node["children"]
