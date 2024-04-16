66e0aa9e32f5b03433a17ea66cfa6ee27ddb403a
Timothy Ryon <tim@timryon.com>
2024-04-15 22:31:39 -0400

    finish method by depth traversal and pass test

--- lib/tree.py
+++ lib/tree.py
@@ -2,6 +2,7 @@ class Tree:
   def __init__(self, root = None):
     self.root = root
 
+# traverse depth method and pass test
   def get_element_by_id(self, id):
     result = list()
     nodes_to_visit = [self.root]
@@ -12,16 +13,18 @@ class Tree:
         return node
       else:
         result.append(node["id"])
-      nodes_to_visit = nodes_to_visit + node["children"]
+      nodes_to_visit = node["children"] + nodes_to_visit
 
-  # def get_element_by_id(self, id, node):
+
+# traverse breadth method and pass test
+  # def get_element_by_id(self, id):
   #   result = list()
-  #   nodes_to_visit = [node]
+  #   nodes_to_visit = [self.root]
 
   #   while len(nodes_to_visit) > 0:
-  #     node = nodes_to_visit.pop[0]
+  #     node = nodes_to_visit.pop(0)
   #     if node["id"] == id:
   #       return node
   #     else:
-  #       result.append(node["text_content"])
+  #       result.append(node["id"])
   #     nodes_to_visit = nodes_to_visit + node["children"]
